b005a2cc3f15ed7ab3f6bcc2d91c93afb6c34688
Morphice <62226449+MorphiceGV@users.noreply.github.com>
2023-05-06 15:46:14 -0400

    comiit to public domain

---
+++ lecture1/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void )
+{
+    printf("Hello, World!\n");
+}
+
+/*
+Functions take in arguments and return values to the caller. 
+
+ */
+
---
+++ sample.py
@@ -0,0 +1,11 @@
+cars = ["ford","volvo","bMw"]
+x = cars[0]
+print(x)
+cars[0] = "toyota"
+print(cars[0])
+
+x = len(cars)
+#inclusive 
+
+cars.append("Chevy")
+
